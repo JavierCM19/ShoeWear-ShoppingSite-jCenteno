079da99ad0547797bd9207827e8d4e3d1df3d859
JavierCM19 <126301772+JavierCM19@users.noreply.github.com>
2025-04-06 11:56:55 -0500

    uploding basic site

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+// NAVBAR OPEN CLOSE FUNCTION
+
+const overlay = document.getElementById("overlay");
+const navOpen = document.getElementById("btn-open");
+const navClose = document.getElementById("btn-close");
+const navbar = document.getElementById("navbar");
+
+overlay.addEventListener("click", function () {
+    navbar.classList.remove("active");
+    overlay.classList.remove("active");
+});
+
+navOpen.addEventListener("click", function () {
+    navbar.classList.add("active");
+    overlay.classList.add("active");
+});
+
+navClose.addEventListener("click", function () {
+    navbar.classList.remove("active");
+    overlay.classList.remove("active");
+});
